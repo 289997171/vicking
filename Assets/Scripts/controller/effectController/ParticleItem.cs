db1ba788edb1c18e895638bd4be26810dbb47445
289997171 <289997171@qq.com>
2016-07-15 12:43:30 +0800

    尝试添加被击特效点，但这种方案不妥，工作量太大

--- Assets/Scripts/controller/effectController/ParticleItem.cs
+++ Assets/Scripts/controller/effectController/ParticleItem.cs
@@ -1,14 +1,16 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 
-public class ParticleItem : MonoBehaviour {
+public class ParticleItem : MonoBehaviour
+{
 
     [SerializeField]
     private List<ParticleSystem> particleSystemsList = new List<ParticleSystem>();
 
     void Start()
     {
+        Debug.LogError("ParticleItem Start()");
+
         ParticleSystem[] particleSystems = this.transform.GetComponentsInChildren<ParticleSystem>();
         particleSystemsList.AddRange(particleSystems);
 
@@ -16,7 +18,116 @@ public class ParticleItem : MonoBehaviour {
         {
             item.startDelay = 0f;
         }
+
+        // if (!this.name.EndsWith("(Clone)")) StartCoroutine(init());
     }
+
+    //#region 将特效直接挂载到对应的节点上
+    //    public string sp_mounts = "Root";
+    //
+    //    private Dictionary<string, Transform> sp_mountsDictionary = new Dictionary<string, Transform>();
+    //
+    //#if UNITY_EDITOR
+    //    public List<Transform> mountsList = new List<Transform>();
+    //#endif
+    //
+    //    public Person person;
+    //
+    //    public Transform personTransform;
+    //
+    //    public ParticleSystemController particleSystemController;
+    //
+    //
+    //    private int maxTryCount = 10;
+    //    private int i = 10;
+    //
+    //    private IEnumerator init()
+    //    {
+    //        yield return 1;
+    //
+    //        Transform parentTra = this.transform.parent;
+    //
+    //        for (;;)
+    //        {
+    //            yield return 1;
+    //
+    //            this.person = parentTra.GetComponent<Person>();
+    //
+    //            if (this.person != null)
+    //            {
+    //                this.personTransform = this.person.transform;
+    //                this.particleSystemController = this.person.GetComponent<ParticleSystemController>();
+    //                break;
+    //            }
+    //
+    //            parentTra = parentTra.parent;
+    //
+    //            if (parentTra == null || i-- < 0)
+    //            {
+    //                
+    //                if (maxTryCount-- < 0)
+    //                {
+    //                    Debug.LogError("无法找到Person节点！！！" + this.name);
+    //                    break;
+    //                }
+    //
+    //                parentTra = this.transform.parent;
+    //            }
+    //
+    //        }
+    //
+    //        if (this.person == null || this.personTransform == null)
+    //        {
+    //            Debug.LogError("无法找到Person节点！！！" + this.name);
+    //            yield break;
+    //        }
+    //
+    //        {
+    //            string[] splits = sp_mounts.Split(';');
+    //            bool addInRoot = splits.Contains("Root");
+    //
+    //            foreach (string split in splits)
+    //            {
+    //                if (string.IsNullOrEmpty(split) || split.Equals("Root"))
+    //                {
+    //                    continue;
+    //                }
+    //
+    //                Transform tra = this.personTransform.findInChildrens(split);
+    //                if (tag == null)
+    //                {
+    //                    Debug.LogError("Person中无法找到：" + split + "节点" + this.name);
+    //                }
+    //                else
+    //                {
+    //                    sp_mountsDictionary.Add(split, tra);
+    //#if UNITY_EDITOR
+    //                    mountsList.Add(tra);
+    //#endif
+    //                    // TODO 需要将自己clone到对应的节点，并设置自己的disable
+    //                    GameObject clone = Instantiate(this.gameObject) as GameObject;
+    //                    Vector3 localPosition = clone.transform.localPosition;
+    //                    Quaternion localRotation = clone.transform.localRotation;
+    //                    Vector3 localScale = clone.transform.localScale;
+    //                    clone.transform.parent = tra;
+    //                    clone.transform.localPosition = localPosition;
+    //                    clone.transform.localRotation = localRotation;
+    //                    clone.transform.localScale = localScale;
+    //
+    //                    this.particleSystemController.particleList.Add(clone.gameObject.GetComponent<ParticleItem>());
+    //                    this.particleSystemController.particleItemMap.Add(clone.name, clone.gameObject.GetComponent<ParticleItem>());
+    //                }
+    //            }
+    //
+    //            if (!addInRoot && !this.name.Contains("(Clone)"))
+    //            {
+    //                Destroy(this.gameObject);
+    //            }
+    //        }
+    //    }
+    //
+    //#endregion
+
 
     public void play()
     {
--- Assets/Scripts/controller/effectController/ParticleSystemController.cs
+++ Assets/Scripts/controller/effectController/ParticleSystemController.cs
@@ -3,17 +3,69 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+[RequireComponent(typeof(Person))]
 public class ParticleSystemController : MonoBehaviour
 {
+
+#if UNITY_EDITOR
     public List<ParticleItem> particleList = new List<ParticleItem>();
+#endif
 
     public Dictionary<string, ParticleItem> particleItemMap = new Dictionary<string, ParticleItem>();
 
+    //    public Person person;
+    //
+    //    public Transform cacheTransform;
+    //
+    //    public string mounts = "Up;Medium;Down;R_Weapon;L_Weapon;Arms01;Bip01 Prop1";
+    //
+    //    public Dictionary<string, Transform> mountsDictionary = new Dictionary<string, Transform>();
+    //
+    //#if UNITY_EDITOR
+    //    public List<Transform> mountsList = new List<Transform>(); 
+    //#endif
+
+    //    public string Up = "Up"; // 血条
+    //    public string Medium = "Medium"; // 被击点
+    //    public string Down = "Down";// 阴影挂点
+    //
+    //    public string R_Weapon = "R_Weapon";// 右手
+    //    public string L_Weapon = "L_Weapon";// 左手
+    //    
+    //    public string Arms01 = "Arms01"; // 背部武器挂点
+    //    public string Bip01_Prop1 = "Bip01 Prop1"; // 武器骨骼,由于是双手武器，挂点要挂在武器骨骼上。
+
+
     void Start()
     {
-        ParticleItem[] items = this.transform.GetComponentsInChildren<ParticleItem>();
-        particleList.AddRange(items);
+        //        {
+        //            this.person = this.GetComponent<Person>();
+        //            this.cacheTransform = this.transform;
+        //            string[] splits = mounts.Split(';');
+        //            foreach (string split in splits)
+        //            {
+        //                Transform tra = this.transform.findInChildrens(split);
+        //                if (tag == null)
+        //                {
+        //                    mountsDictionary.Add(split, cacheTransform); // 找不到节点，默认以
+        //                }
+        //                else
+        //                {
+        //                    mountsDictionary.Add(split, tra);
+        //#if UNITY_EDITOR
+        //                    mountsList.Add(tra);
+        //#endif
+        //                }
+        //            }
+        //        }
+        //        
 
+        ParticleItem[] items = this.transform.GetComponentsInChildren<ParticleItem>();
+
+#if UNITY_EDITOR
+        particleList.AddRange(items);
+#endif
         foreach (ParticleItem item in items)
         {
             particleItemMap.Add(item.name, item);
@@ -21,28 +73,16 @@ public class ParticleSystemController : MonoBehaviour
             //item.gameObject.SetActive(false);
         }
 
-//        StartCoroutine(test());
     }
 
-//    private IEnumerator test()
-//    {
-//        yield return new WaitForSeconds(5);
-//        playParticle("skill_00_01");
-//        yield return new WaitForSeconds(5);
-//        playParticle("skill_00_01");
-//        yield return new WaitForSeconds(5);
-//        playParticle("skill_00_01");
-//        yield return new WaitForSeconds(5);
-//        playParticle("skill_00_01");
-//    }
+
+
+
 
     public void playParticle(string particleInfo)
     {
-        //Debug.LogError("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
-        string[] splits = particleInfo.Split(';');
-
+        string[] splits = particleInfo.Split(',');
         string particleName = splits[0];
-
 
         ParticleItem particleItem;
         if (particleItemMap.TryGetValue(particleName, out particleItem))
@@ -50,6 +90,5 @@ public class ParticleSystemController : MonoBehaviour
             //particleItem.gameObject.SetActive(true);
             particleItem.play();
         }
-
     }
 }
